188e92f16975d46ea8a065a81deaff54d3da8d77
Diego Da'Silva <smartcrashomg@gmail.com>
2020-12-04 16:54:03 -0400

    Add `START` button and algorithnm selector

--- src/app.jsx
+++ src/app.jsx
@@ -1,25 +1,64 @@
+import { useState } from 'preact/hooks'
+
+import * as algorithms from './algorithms'
 import { Canvas } from './components/Canvas'
 import { Grid } from './classes/grid'
 import { Node } from './classes/node'
-import { dijkstras } from './algorithms/dijkstras'
-import { aStar } from './algorithms/a-star'
 
 const grid = new Grid(25, 25, (_, index) => new Node({ index }))
-const start = grid.array.get(0, 0)
-const end = grid.array.get(0, 24)
-
-const { states, path } = aStar(grid, start, end)
 
 export function App() {
+  const [start, setStart] = useState(grid.array.get(0, 0))
+  const [end, setEnd] = useState(grid.array.get(2, 0))
+  const [algorithm, setAlgorithm] = useState(Object.keys(algorithms)[0])
+  const [states, setStates] = useState(null)
+  const [path, setPath] = useState(null)
+
+  const [isPaused, setIsPaused] = useState(true)
+  const [hasFinished, setHasFinished] = useState(false)
+
+  const onButtonClick = () => {
+    if (isPaused) {
+      const { states, path } = algorithms[algorithm](grid, start, end)
+
+      setStates(states)
+      setPath(path)
+      setIsPaused(false)
+      setHasFinished(false)
+    }
+
+    if (hasFinished) {
+      setStates(null)
+      setPath(null)
+      setIsPaused(true)
+      setHasFinished(false)
+    }
+  }
+
   return (
-    <main class="flex items-center justify-center h-screen">
+    <main class="flex items-center justify-center flex-col">
+      <h1>{algorithm}</h1>
+
+      <button onClick={onButtonClick}>
+        {hasFinished ? 'RESTART' : isPaused ? 'START' : 'RUNNING'}
+      </button>
+
+      <select onChange={e => setAlgorithm(e.target.value)}>
+        {Object.keys(algorithms).map(key => (
+          <option value={key}>{key}</option>
+        ))}
+      </select>
+
       <Canvas
+        paused={isPaused}
         rows={grid.rows}
         columns={grid.columns}
         start={start}
         end={end}
         states={states}
         path={path}
+        onClick={({ x, y }) => setEnd(grid.array.get(y, x) || end)}
+        onFinish={() => setHasFinished(true)}
       />
     </main>
   )
--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -4,27 +4,63 @@ import Sketch from 'react-p5'
 const SIZE = 18
 let index = 0
 
-export function Canvas({ rows, columns, start, end, states, path }) {
+export function Canvas({
+  paused,
+  rows,
+  columns,
+  start,
+  end,
+  states,
+  path,
+  onClick = () => null,
+  onFinish = () => null,
+}) {
   const setup = (p5, canvasParentRef) => {
     p5.createCanvas(rows * SIZE, columns * SIZE).parent(canvasParentRef)
     p5.background(255)
-    p5.stroke(180)
     p5.frameRate(35)
   }
 
   const draw = p5 => {
+    p5.stroke(180)
+    p5.strokeWeight(1)
+
+    if (paused) {
+      p5.stroke(180)
+      p5.fill(255)
+      p5.rect(0, 0, p5.width, p5.height, SIZE / 2)
+
+      p5.fill(0, 255, 0)
+      p5.ellipse(
+        end.coordinates(rows, columns).x * SIZE + SIZE / 2,
+        end.coordinates(rows, columns).y * SIZE + SIZE / 2,
+        SIZE / 2
+      )
+
+      p5.fill('red')
+      p5.ellipse(
+        start.coordinates(rows, columns).x * SIZE + SIZE / 2,
+        start.coordinates(rows, columns).y * SIZE + SIZE / 2,
+        SIZE / 2
+      )
+
+      return
+    }
+
     if (index === states.length) {
-      const nodes = path.map(node => node.coordinates(rows, columns))
       p5.noFill()
       p5.stroke(0, 0, 255)
       p5.beginShape()
       p5.strokeWeight(SIZE / 2)
 
-      for (const { x, y } of nodes)
+      for (const node of path) {
+        const { x, y } = node.coordinates(rows, columns)
         p5.vertex(x * SIZE + SIZE / 2, y * SIZE + SIZE / 2)
+      }
 
       p5.endShape()
-      p5.noLoop()
+
+      onFinish()
 
       return
     }
@@ -56,12 +92,17 @@ export function Canvas({ rows, columns, start, end, states, path }) {
       SIZE / 2
     )
 
-    const { x, y } = end.coordinates(rows, columns)
-    p5.fill(0, 255, 0)
-    p5.ellipse(x * SIZE + SIZE / 2, y * SIZE + SIZE / 2, SIZE / 2)
-
     ++index
   }
 
-  return <Sketch setup={setup} draw={draw} />
+  const mouseClicked = ({ mouseX, mouseY, width, height }) => {
+    if (mouseX >= 0 && mouseX <= width && mouseY >= 0 && mouseY <= height) {
+      const y = Math.floor(mouseY / SIZE)
+      const x = Math.floor(mouseX / SIZE)
+
+      onClick({ x, y })
+    }
+  }
+
+  return <Sketch setup={setup} draw={draw} mouseClicked={mouseClicked} />
 }
